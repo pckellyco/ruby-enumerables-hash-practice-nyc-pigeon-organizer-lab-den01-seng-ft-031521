40221653af769fd9f7cba6555a1eddf61a81c040
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 15:46:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  hash.each_with_object({}) do |(key, value), final_array|
+  #write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
     binding.pry
     final_array
   end
